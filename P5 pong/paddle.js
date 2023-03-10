0566937ee1b5d82c21152d671040f2ab75a0d6a2
Isael742 <62968411+Isael742@users.noreply.github.com>
2023-03-10 11:58:25 -0600

    Add files via upload

--- P5 pong/paddle.js
+++ P5 pong/paddle.js
@@ -1,8 +1,10 @@
-class paddle{
-    constructor(idLeft){
+class Paddle{
+    constructor(isLeft){
         this.w = 25;
         this.h = 100;
         this.y = int(height / 2);
+        this.step = 0;
+
         if(isLeft){
             this.x = 0;
         }else{ 
@@ -11,6 +13,14 @@ class paddle{
     }
 
     draw(){
-        
+        rect(this.x, this.y, this.w, this.h);
+    }
+
+    move(dir){
+        this.step = dir;
+    }
+
+    update(){
+        this.y += this.step;
     }
 }
